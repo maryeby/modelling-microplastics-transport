e2d594ae308ed4acd84018c41f7af23364eb0f58
Mary Eby <m.eby@sms.ed.ac.uk>
2023-02-15 12:52:38 +0000

    WIP: following approach from Santamaria 2013

--- src/santamaria_model/compare_drift_velocities.py
+++ src/santamaria_model/compare_drift_velocities.py
@@ -4,88 +4,222 @@ import scipy.integrate as integrate
 
 import santamaria
 
-my_wave = santamaria.SantamariaModel(wave_num=2 * np.pi, amplitude=0.02,
-							 		 stokes_num=0.157, beta=0.9)
-# defining local parameters for ease of use
-U = my_wave.get_max_velocity()
-omega = my_wave.get_angular_freq()
-k = my_wave.get_wave_num()
+def main():
+	delta_t = 5e-5
+	is_numerical=True
+	my_wave = santamaria.SantamariaModel(wave_num=2 * np.pi, amplitude=0.02,
+										 stokes_num=0.157, beta=0.9)
+	x, z, u, w, t = my_wave.particle_trajectory(delta_t=delta_t)
 
-# compute drift velocities
-x, z, u, w, t = my_wave.particle_trajectory(0, 0)
-previous_t, current_t, previous_u, current_u, previous_w, current_w, \
-	interpd_t, interpd_u, interpd_w, \
-	interpd_x, interpd_z = my_wave.period_info(x, z, u, w, t)
-numerical_u, numerical_w, numerical_t, analytical_ud, analytical_wd, \
-	analytical_u, analytical_w = my_wave.drift_velocities(x, z, u, w, t)
+	plt.figure(1)
+#	plot_uw(my_wave, x, z, u, w, t, is_numerical)
+#	plt.figure(2)
+#	plot_particle_trajectory(my_wave, x, z, u, w, t)
+#	plt.figure(3)
+	plot_drift_velocity(my_wave, x, z, u, w, t, delta_t, limit_axes=False)
+#	plot_drift_velocity_varying_delta_t(my_wave, x, z, u, w, t,
+#										limit_axes=False)
+	plt.show()
 
-# plot results
-plt.figure(1)
-plt.subplot(211)
-plt.title('Horizontal Lagrangian Velocity with Period Endpoints', fontsize=14)
-plt.xlabel(r'$ \omega t $', fontsize=12)
-plt.ylabel(r'$ u / U $', fontsize=12)
-plt.axhline(0, c='silver', linestyle=':')
-plt.plot(omega * t, u / U, 'k', zorder=2, label='scaled horizontal velocity')
-plt.plot(omega * t, analytical_u / U, c='coral', zorder=1,
-		 label='scaled analytical u')
-plt.scatter(omega * np.array(current_t), np.array(current_u) / U, c='k',
-			zorder=4, label='chosen period endpoint')
-plt.scatter(omega * np.array(previous_t), np.array(previous_u) / U, c='coral',
-			zorder=3, label='last point before period endpoint')
-plt.scatter(omega * np.array(interpd_t), np.array(interpd_u) / U,
-			c='mediumpurple', zorder=5, label='interpolated period endpoint')
-plt.legend()
+def plot_uw(my_wave, x, z, u, w, t, is_numerical):
+	U = my_wave.get_max_velocity()
+	omega = my_wave.get_angular_freq()
+	analytical_u, analytical_w = my_wave.analytical_particle_velocity(t=t)
 
-plt.subplot(212)
-plt.title('Vertical Lagrangian Velocity with Period Endpoints', fontsize=14)
-plt.xlabel(r'$ \omega t $', fontsize=12)
-plt.ylabel(r'$ w / U $', fontsize=12)
-plt.axhline(my_wave.get_settling_velocity() / U, c='k', zorder=1, linestyle=':')
-plt.plot(omega * t, w / U, 'k', zorder=3, label='scaled vertical velocity')
-plt.plot(omega * t, analytical_w / U, c='coral', zorder=2,
-		 label='scaled analytical w')
-plt.scatter(omega * np.array(current_t), np.array(current_w) / U, c='k',
-			zorder=5, label='chosen period endpoint')
-plt.scatter(omega * np.array(previous_t), np.array(previous_w) / U, c='coral',
-			zorder=4, label='last point before period endpoint')
-plt.scatter(omega * np.array(interpd_t), np.array(interpd_w) / U,
-			c='mediumpurple', zorder=6, label='interpolated period endpoint')
-plt.legend()
+	plt.subplot(211)
+	plt.title('Horizontal Lagrangian Velocity with Period Endpoints',
+			  fontsize=14)
+	plt.xlabel(r'$ \omega t $', fontsize=12)
+	plt.ylabel(r'$ u / U $', fontsize=12)
+	plt.axhline(0, c='silver', zorder=1, linestyle=':')
+	plt.plot(omega * t, u / U, 'k', zorder=3,
+			 label='scaled horizontal velocity')
+	plt.plot(omega * t, analytical_u / U, c='coral', zorder=2,
+			 label='scaled analytical u')
 
-#plt.figure(2)
-plt.suptitle('Drift Velocity Comparison', fontsize=16)
-plt.subplot(121)
-plt.xlabel(r'$ \omega t $', fontsize=12)
-plt.ylabel(r'$ u_d / U $', fontsize=12)
-plt.xlim(0, 80)
-plt.ylim(0, 0.15)
-plt.xticks(ticks=range(0, 80, 10))
-plt.yticks(ticks=[0, 0.05, 0.1])
-plt.plot(my_wave.get_angular_freq() * t, analytical_ud / U, c='k',
-		 label='analytical')
-plt.scatter(omega * np.array(numerical_t), numerical_u / U, facecolors='none', 
-			edgecolors='k', label='numerical')
+	plt.subplot(212)
+	plt.title('Vertical Lagrangian Velocity with Period Endpoints', fontsize=14)
+	plt.xlabel(r'$ \omega t $', fontsize=12)
+	plt.ylabel(r'$ w / U $', fontsize=12)
+	plt.axhline(my_wave.get_settling_velocity() / U, c='k', zorder=1,
+				linestyle=':')
+	plt.plot(omega * t, w / U, 'k', zorder=3, label='scaled vertical velocity')
+	plt.plot(omega * t, analytical_w / U, c='coral', zorder=2,
+			 label='scaled analytical w')
 
-plt.subplot(122)
-plt.xlabel(r'$ \omega t $', fontsize=12)
-plt.ylabel(r'$ w_d / U $', fontsize=12)
-plt.xlim(0, 80)
-plt.ylim(-0.128, -0.1245)
-plt.xticks(ticks=range(0, 80, 10))
-plt.yticks(ticks=[-0.128, -0.127, -0.126, -0.125])
-plt.axhline(my_wave.get_settling_velocity() / U, c='k', linestyle=':')
-plt.plot(omega * t, analytical_wd / U, c='k', label='analytical')
-plt.scatter(omega * np.array(numerical_t), np.array(numerical_w) / U,
-			facecolors='none', edgecolors='k', label='numerical')
+	if is_numerical:
+		previous_t, current_t, previous_u, current_u, previous_w, current_w, \
+			interpd_t, interpd_u, interpd_w, interpd_x, \
+			interpd_z = my_wave.interpolated_period_info(x, z, u, w, t)
 
-plt.figure(3)
-plt.title('Particle Trajectory', fontsize=16)
-plt.xlabel('kx', fontsize='12')
-plt.ylabel('kz', fontsize='12')
-plt.plot(k * x, k * z, c='k', marker='.', zorder=3,
-		 label='generated data points')
-plt.scatter(k * np.array(interpd_x), k * np.array(interpd_z), c='mediumpurple',
-			marker='o', zorder=4, label='period endpoints')
-plt.legend()
-plt.show()
+		plt.subplot(211)
+		plt.scatter(omega * np.array(current_t), np.array(current_u) / U, c='k',
+					zorder=5, label='chosen period endpoint')
+		plt.scatter(omega * np.array(previous_t), np.array(previous_u) / U,
+					c='coral', zorder=4,
+					label='last point before period endpoint')
+		plt.scatter(omega * np.array(interpd_t), np.array(interpd_u) / U,
+					c='mediumpurple', zorder=6,
+					label='interpolated period endpoint')
+		plt.legend()
+
+		plt.subplot(212)
+		plt.scatter(omega * np.array(current_t), np.array(current_w) / U, c='k',
+					zorder=5, label='chosen period endpoint')
+		plt.scatter(omega * np.array(previous_t), np.array(previous_w) / U,
+					c='coral', zorder=4,
+					label='last point before period endpoint')
+		plt.scatter(omega * np.array(interpd_t), np.array(interpd_w) / U,
+					c='mediumpurple', zorder=6,
+					label='interpolated period endpoint')
+		plt.legend()
+	else:
+		_, _, _, current_u, current_w, \
+			current_t = my_wave.analytical_period_info(x, z, t)
+		plt.subplot(211)
+		plt.scatter(omega * np.array(current_t), np.array(current_u) / U, c='k',
+					zorder=4, label='chosen period endpoint')
+		plt.legend()
+
+		plt.subplot(212)
+		plt.scatter(omega * np.array(current_t), np.array(current_w) / U, c='k',
+					zorder=4, label='chosen period endpoint')
+		plt.legend()
+
+def plot_particle_trajectory(my_wave, x, z, u, w, t):
+	k = my_wave.get_wave_num()
+	_, _, _, _, _, _, _, _, _, \
+	numerical_x, numerical_z = my_wave.interpolated_period_info(x, z, u,
+																w, t)
+	_, analytical_x, analytical_z, \
+	_, _, _ = my_wave.analytical_period_info(x, z, t)
+
+	plt.title('Particle Trajectory', fontsize=16)
+	plt.xlabel('kx', fontsize='12')
+	plt.ylabel('kz', fontsize='12')
+	plt.plot(k * x, k * z, c='k', marker='.', zorder=1,
+			 label='generated data points')
+	plt.scatter(k * np.array(numerical_x), k * np.array(numerical_z),
+				c='mediumpurple', zorder=3,
+				label='numerical period endpoints')
+	plt.scatter(k * np.array(analytical_x), k * np.array(analytical_z),
+				c='coral', zorder=2,
+				label='analytical period endpoints')
+	plt.legend()
+
+def plot_drift_velocity(my_wave, x, z, u, w, t, delta_t, limit_axes):
+	U = my_wave.get_max_velocity()
+	omega = my_wave.get_angular_freq()
+	analytical_ud, analytical_wd = my_wave.analytical_drift_velocity(t=t)
+	my_ud, my_wd = my_wave.my_analytical_drift_velocity(z=z, u=u, w=w, t=t)
+	u_d, w_d, numerical_t = my_wave.numerical_drift_velocity(x, z, u, w, t)
+	u_avg, w_avg, analytical_t = my_wave.analytical_averages(x, z, u, w, t)
+
+	plt.suptitle('Drift Velocity Comparison with '
+				 + r'$\Delta t =$ {:.0e}'.format(delta_t), fontsize=16)
+	plt.subplot(121)
+	plt.xlabel(r'$ \omega t $', fontsize=12)
+	plt.ylabel(r'$ u_d / U $', fontsize=12)
+	plt.plot(omega * t, analytical_ud / U, c='k', zorder=1, 
+			 label='eq (13) as listed in Santamaria 2013')
+	plt.plot(omega * t, my_ud / U, c='mediumpurple', zorder=1,
+			 label='eq (13) from our calculations')
+	plt.scatter(omega * numerical_t, u_d / U, zorder=3, 
+				facecolors='none', edgecolors='k', label='numerical')
+#	plt.scatter(omega * analytical_t, u_avg / U, zorder=2, c='k', marker='x',
+#				label='average of eq (11)')
+	plt.legend()
+
+	plt.subplot(122)
+	plt.xlabel(r'$ \omega t $', fontsize=12)
+	plt.ylabel(r'$ w_d / U $', fontsize=12)
+	plt.axhline(my_wave.get_settling_velocity() / U, c='k', linestyle=':')
+	plt.plot(omega * t, analytical_wd / U, c='k', zorder=1,
+			 label='eq (14) as listed in Santamaria 2013')
+	plt.plot(omega * t, my_wd / U, c='mediumpurple', zorder=1,
+			 label='eq (14) from our calculations')
+	plt.scatter(omega * numerical_t, w_d / U, zorder=3, 
+				facecolors='none', edgecolors='k', label='numerical')
+#	plt.scatter(omega * analytical_t, w_avg / U, zorder=2, c='k', marker='x',
+#				label='average of eq (12)')
+	plt.legend()
+
+	if limit_axes:
+		plt.subplot(121)
+		plt.xlim(0, 80)
+		plt.ylim(0, 0.15)
+		plt.xticks(ticks=range(0, 80, 10))
+		plt.yticks(ticks=[0, 0.05, 0.1])
+		
+		plt.subplot(122)
+		plt.xlim(0, 80)
+		plt.ylim(-0.128, -0.1245)
+		plt.xticks(ticks=range(0, 80, 10))
+		plt.yticks(ticks=[-0.128, -0.127, -0.126, -0.125])
+
+def plot_drift_velocity_varying_delta_t(my_wave, x, z, u, w, t, limit_axes):
+	U = my_wave.get_max_velocity()
+	omega = my_wave.get_angular_freq()
+
+	# coarse delta t
+	x_coarse, z_coarse, u_coarse, w_coarse, \
+		t_coarse = my_wave.particle_trajectory(delta_t=5e-3)
+	numerical_u_coarse, numerical_w_coarse, \
+		numerical_t_coarse = my_wave.numerical_drift_velocity(x_coarse, 
+								 z_coarse, u_coarse, w_coarse, t_coarse)
+	# medium delta t
+	analytical_ud, analytical_wd = my_wave.analytical_drift_velocity(t=t)
+	numerical_u_med, numerical_w_med, \
+		numerical_t_med = my_wave.numerical_drift_velocity(x, z, u, w, t)
+
+	# fine delta t
+	x_fine, z_fine, u_fine, w_fine, \
+		t_fine = my_wave.particle_trajectory(delta_t=5e-7)
+	numerical_u_fine, numerical_w_fine, \
+		numerical_t_fine = my_wave.numerical_drift_velocity(x_fine,
+							   z_fine, u_fine, w_fine, t_fine)
+	
+	plt.suptitle(r'Drift Velocity Comparison with Varying $\Delta t$',
+				 fontsize=16)
+	plt.subplot(121)
+	plt.xlabel(r'$ \omega t $', fontsize=12)
+	plt.ylabel(r'$ u_d / U $', fontsize=12)
+	plt.plot(omega * t, analytical_ud / U, c='k', zorder=1, label='analytical')
+	plt.scatter(omega * numerical_t_coarse, numerical_u_coarse / U,
+				zorder=2, marker='s', facecolors='none', edgecolors='k',
+				label=r'coarse $\Delta t$')
+	plt.scatter(omega * numerical_t_med, numerical_u_med / U, zorder=2, 
+				facecolors='none', edgecolors='k', label=r'medium $\Delta t$')
+	plt.scatter(omega * numerical_t_fine, numerical_u_fine / U, zorder=4,
+				c='k', marker='x', label=r'fine $\Delta t$')
+	plt.legend()
+
+	plt.subplot(122)
+	plt.xlabel(r'$ \omega t $', fontsize=12)
+	plt.ylabel(r'$ w_d / U $', fontsize=12)
+	plt.axhline(my_wave.get_settling_velocity() / U, c='k', linestyle=':')
+	plt.plot(omega * t, analytical_wd / U, c='k', zorder=1, label='analytical')
+	plt.scatter(omega * numerical_t_coarse,
+				numerical_w_coarse / U, zorder=2,
+				marker='s', facecolors='none', edgecolors='k',
+				label=r'coarse $\Delta t$')
+	plt.scatter(omega * numerical_t_med, numerical_w_med / U, zorder=3,
+				facecolors='none', edgecolors='k', label=r'medium $\Delta t$')
+	plt.scatter(omega * numerical_t_fine, numerical_w_fine / U,
+				zorder=4, c='k', marker='x', label=r'fine $\Delta t$')
+	plt.legend()
+
+	if limit_axes:
+		plt.subplot(121)
+		plt.xlim(0, 80)
+		plt.ylim(0, 0.15)
+		plt.xticks(ticks=range(0, 80, 10))
+		plt.yticks(ticks=[0, 0.05, 0.1])
+		
+		plt.subplot(122)
+		plt.xlim(0, 80)
+		plt.ylim(-0.128, -0.1245)
+		plt.xticks(ticks=range(0, 80, 10))
+		plt.yticks(ticks=[-0.128, -0.127, -0.126, -0.125])
+
+main()
--- src/santamaria_model/santamaria.py
+++ src/santamaria_model/santamaria.py
@@ -39,53 +39,90 @@ class SantamariaModel:
 								  - self.__angular_freq * t)])
 
 	def fluid_derivative(self, x, z, t):
-		fluid_velocity = self.fluid_velocity(x, z, t)
-		return np.array([self.__angular_freq * self.__max_velocity
-						* np.exp(self.__wave_num * z)
-						* np.sin(self.__wave_num * x - self.__angular_freq * t),
-						-self.__angular_freq * self.__max_velocity
-						* np.exp(self.__wave_num * z)
-						* np.cos(self.__wave_num * x - self.__angular_freq * t)]
-						+ np.dot(fluid_velocity, np.gradient(fluid_velocity)))
+		u, w = self.fluid_velocity(x, z, t)
+		return np.array([self.__angular_freq * w,
+						 self.__wave_num * self.__max_velocity ** 2
+										 * np.exp(2 * self.__wave_num * z)
+										 - self.__angular_freq * u])
+#		return np.array([self.__angular_freq * self.__max_velocity
+#						* np.exp(self.__wave_num * z)
+#						* np.sin(self.__wave_num * x - self.__angular_freq * t),
+#						-self.__angular_freq * self.__max_velocity
+#						* np.exp(self.__wave_num * z)
+#						* np.cos(self.__wave_num * x - self.__angular_freq * t)]
+#						+ np.dot(fluid_velocity,
+#								 np.gradient(1/2 * fluid_velocity ** 2)))
 
-	def analytical_velocities(self, x_0=0, z_0=0, t=0):
+	def analytical_particle_velocity(self, x_0=0, z_0=0, t=0):
 		# local variables for equations (11) and (12)
 		bprime = 1 - self.__beta
 		phi = self.__wave_num * x_0 - self.__angular_freq * t
-		z_0t = z_0 - self.__stokes_num * bprime * t
+		z_0t = z_0 - self.__stokes_num * self.__phase_velocity * bprime * t
 
 		# equation (11)
-		analytical_u = self.__max_velocity * np.exp(self.__wave_num * z_0t) \
-											 * ((1 - self.__stokes_num ** 2
-												   * self.__beta * bprime)
-												   * np.cos(phi)
-												   - self.__stokes_num * bprime
-												   * np.sin(phi)) \
-											 + self.__max_velocity ** 2 \
-											 / self.__phase_velocity \
-											 * np.exp(2 * self.__wave_num
-														* z_0t) \
-											 * (1 - self.__stokes_num ** 2
-												  * self.__beta * bprime)
+		u = self.__max_velocity * np.exp(self.__wave_num * z_0t) \
+								* ((1 - self.__stokes_num ** 2 * self.__beta
+									  * bprime) * np.cos(phi) \
+									  - self.__stokes_num * bprime \
+									  * np.sin(phi)) \
+								+ self.__max_velocity ** 2 \
+								/ self.__phase_velocity \
+								* np.exp(2 * self.__wave_num * z_0t) \
+								* (1 - self.__stokes_num ** 2 * self.__beta
+									 * bprime)
 		# equation (12)
-		analytical_w = self.__max_velocity * np.exp(self.__wave_num * z_0t) \
-										   * ((1 - self.__stokes_num ** 2
-												 * self.__beta * bprime)
-												 * np.sin(phi)
-												 + self.__stokes_num * bprime
-												 * np.cos(phi)) \
-										   - self.__phase_velocity \
-										   * self.__stokes_num * bprime \
-										   * (1 + 2 * self.__max_velocity ** 2
-												/ self.__phase_velocity ** 2
-												* np.exp(2 * self.__wave_num
-														   * z_0t))
-		return analytical_u, analytical_w
+		w = self.__max_velocity * np.exp(self.__wave_num * z_0t) \
+								* ((1 - self.__stokes_num ** 2 * self.__beta
+									  * bprime)
+								* np.sin(phi) + self.__stokes_num * bprime \
+								* np.cos(phi)) \
+								- self.__phase_velocity \
+								* self.__stokes_num * bprime \
+								* (1 + 2 * self.__max_velocity ** 2
+								/ self.__phase_velocity ** 2
+								* np.exp(2 * self.__wave_num * z_0t))
+		return u, w
 
-	def particle_trajectory(self, x_0, z_0, method='BDF'):
-		t_span = (0, 200 * self.__period)
-		t_eval = np.linspace(0, 50 * self.__period, 7500)
-		u_0, w_0 = self.analytical_velocities()
+	def analytical_drift_velocity(self, x_0=0, z_0=0, t=0):
+		bprime = 1 - self.__beta
+
+		# equation (13)
+		u_d = self.__max_velocity ** 2 / self.__phase_velocity \
+				  * (1 - self.__beta * bprime * self.__stokes_num ** 2) \
+				  * np.exp(2 * self.__wave_num * (z_0 - bprime * self.__gravity 
+				  * self.__st_response_time * t))
+#		delta = 40 / self.__angular_freq	# shift anomaly in w_d
+		# equation (14)
+		w_d = -bprime * self.__gravity * self.__st_response_time \
+				  - 2 * bprime * self.__stokes_num \
+				  * self.__max_velocity ** 2 / self.__phase_velocity \
+				  * np.exp(2 * self.__wave_num * (z_0 - bprime * self.__gravity
+							 * self.__st_response_time * t))
+#							 * self.__st_response_time * (t - delta)))
+		return u_d, w_d
+
+	def my_analytical_drift_velocity(self, x_0=0, z_0=0, z=0, u=0, w=0, t=0):
+		bprime = 1 - self.__beta
+		e_2z0t = np.exp(2 * (self.__wave_num * z_0 - self.__stokes_num * bprime
+											 * t * self.__angular_freq))
+
+		# equation (13) derived using my equations (6) and (7)
+		u_d = e_2z0t * (1 - bprime * self.__stokes_num ** 2) \
+					 * (self.__max_velocity ** 2 / self.__phase_velocity)
+
+		# equation (14) derived using my equations (6) and (7)
+		w_d = -self.__phase_velocity * self.__stokes_num * bprime \
+				* (1 + 2 * self.__max_velocity ** 2 
+					 / self.__phase_velocity ** 2 * e_2z0t)
+
+		return u_d, w_d
+
+	def particle_trajectory(self, x_0=0, z_0=0, delta_t=5e-3, method='BDF'):
+		num_periods = 50
+		num_steps = int(np.rint(num_periods * self.__period / delta_t))
+		t_span = (0, num_periods * self.__period)
+		t_eval = np.linspace(0, num_periods * self.__period, num_steps)
+		u_0, w_0 = self.analytical_particle_velocity()
 		sols = integrate.solve_ivp(self.model, t_span, [x_0, z_0, u_0, w_0],
 								   method=method, t_eval=t_eval, rtol=1e-8,
 								   atol=1e-10)
@@ -93,14 +130,49 @@ class SantamariaModel:
 		t = sols.t
 		return x, z, u, w, t
 
-	def period_info(self, x, z, u, w, t):
-#		u, w = self.analytical_velocities(t=t)
+	def analytical_period_info(self, x, z, t):
+		# TODO find t's where analytical u = 0 at x_0, z_0 = 0, 0
+		# TODO save into list period_end_t
+		# TODO make list of zeros called period_end_u the same length
+		# TODO compute w's at each t, save into period_end_w list
+
+		u, w = self.analytical_particle_velocity(t=t)
 		period_end = []
 		for i in range(1, len(u)):
 			if u[i - 1] < 0 and 0 <= u[i]:
 				period_end.append(i)
 
-#		return period_end
+		current_x, current_z, current_u, current_w, current_t = [], [], [], \
+															    [], []
+		for i in range(1, len(period_end)):
+			current = period_end[i]
+			current_x.append(x[current])
+			current_z.append(z[current])
+			current_u.append(u[current])
+			current_w.append(w[current])
+			current_t.append(t[current])
+
+		return period_end, np.array(current_x), np.array(current_z), \
+			   np.array(current_u), np.array(current_w), np.array(current_t)
+
+	def analytical_averages(self, x, z, u, w, t):
+		averaged_u, averaged_w = [], []
+		period_end, _, _, _, _, period_t = self.analytical_period_info(x, z, t)
+		analytical_u, analytical_w = self.analytical_particle_velocity(t=t)
+
+		for i in range(1, len(period_end)):
+			previous = period_end[i - 1]
+			current = period_end[i]
+			averaged_u.append(np.average(analytical_u[previous:current]))
+			averaged_w.append(np.average(analytical_w[previous:current]))
+
+		return np.array(averaged_u), np.array(averaged_w), np.array(period_t)
+
+	def interpolated_period_info(self, x, z, u, w, t):
+		period_end = []
+		for i in range(1, len(u)):
+			if u[i - 1] < 0 and 0 <= u[i]:
+				period_end.append(i)
 
 		previous_t, current_t, previous_u, current_u, previous_w, current_w, \
 			interpd_t, interpd_u, interpd_w, \
@@ -129,97 +201,25 @@ class SantamariaModel:
 			interpd_z.append(np.interp(new_t, [t[previous], t[current]],
 							 [z[previous], z[current]]))
 
-		return previous_t, current_t, previous_u, current_u, previous_w, \
-			   current_w, interpd_t, interpd_u, interpd_w, interpd_x, interpd_z
+		return np.array(previous_t), np.array(current_t), np.array(previous_u),\
+			   np.array(current_u), np.array(previous_w), \
+			   np.array(current_w), np.array(interpd_t), np.array(interpd_u), \
+			   np.array(interpd_w), np.array(interpd_x), np.array(interpd_z)
 
-	def drift_velocities(self, x, z, u, w, t, x_0=0, z_0=0):
-		numerical_u, numerical_w, numerical_t = [], [], []
+	def numerical_drift_velocity(self, x, z, u, w, t, x_0=0, z_0=0):
+		numerical_u, numerical_w = [], []
 		previous_t, current_t, previous_u, current_u, previous_w, current_w, \
 			interpd_t, interpd_u, interpd_w, \
-			interpd_x, interpd_z = self.period_info(x, z, u, w, t)
-		analytical_u, analytical_w = self.analytical_velocities(t=t)
-#		period_end = self.period_info(x, z, u, w, t)
+			interpd_x, interpd_z = self.interpolated_period_info(x, z, u, w, t)
 
 		for i in range(1, len(interpd_t)):
-#		for i in range(1, len(period_end)):
-			previous = period_end[i - 1]
-			current = period_end[i]
 			numerical_u.append((interpd_x[i] - interpd_x[i - 1]) \
 							   / (interpd_t[i] - interpd_t[i - 1]))
 			numerical_w.append((interpd_z[i] - interpd_z[i - 1]) \
 							   / (interpd_t[i] - interpd_t[i - 1]))
-#			numerical_u.append(np.average(analytical_u[previous:current]))
-#			numerical_w.append(np.average(analytical_w[previous:current]))
-			numerical_t.append(interpd_t[i])
-#			numerical_t.append(t[current])
 
-		# pinpointing where the problematic value arises
-		high_w = [num for num in numerical_w \
-				  if num / self.__max_velocity > -0.123][0]
-		print(f'%-15s%-10s' % ('u_d', 't'))
-		for i in range(len(numerical_w)):
-			if numerical_w[i] == high_w:
-				print(f'%-15f%-10f ***' % (numerical_w[i], numerical_t[i]))
-			else:
-				print(f'%-15f%-10f' % (numerical_w[i], numerical_t[i]))
-
-		# curve fitting to w as an attempt to compute the avg using integration
-#		f = lambda x, a, b : np.exp(a * x) * np.cos(b * x)
-#		a, b = curve_fit(f, t, w)[0]
-
-#		numerical_w, numerical_t = [], []
-#		period = 2 * np.pi / self.__angular_freq
-
-#		test_result = integrate.quad(f, 0, period, args=(a, b,))[0]
-#		print(test_result)
-
-#		for num in range(15):
-#			c = num * period
-#			d = c + period
-#			numerical_w.append(integrate.quad(f, c, d, args=(a, b,))[0]
-#							   / period)
-#			numerical_t.append(d)
-
-		# equation (13)
-		analytical_ud = self.__max_velocity ** 2 / self.__phase_velocity \
-					   * (1 - self.__beta * (1 - self.__beta)
-							* self.__stokes_num ** 2) \
-					   * np.exp(2 * self.__wave_num * (z_0 - (1 - self.__beta)
-								  * self.__gravity * self.__st_response_time
-								  * t))
-		delta = 40 / self.__angular_freq	# shift anomaly in w_d
-		# equation (14)
-		analytical_wd = -(1 - self.__beta) * self.__gravity \
-					   * self.__st_response_time - 2 * (1 - self.__beta) \
-					   * self.__stokes_num * self.__max_velocity ** 2 \
-					   / self.__phase_velocity * np.exp(2 * self.__wave_num
-					   * (z_0 - (1 - self.__beta) * self.__gravity
-					   * self.__st_response_time * t))
-#					   * self.__st_response_time * (t - delta)))
-
-		# trying to find values of u near omega * t = 100
-#		near_100 = self.__angular_freq * np.array(numerical_t) - 100
-#		near_100_results = (np.abs(near_100) < 5).nonzero()[0]
-#		print('numerical u_d near omega * t = 100')
-#		print(f'%-22s%-25s' % ('u_d / U', 'omega * t'))
-#		for i in near_100_results:
-#			print(f'%-22f%-25f' % (numerical_u[i] / self.__max_velocity,
-#								 self.__angular_freq * numerical_t[i]))
-#		numerical_result = numerical_u[near_100_results[0]]
-
-#		near_100 = self.__angular_freq * np.array(t) - 100
-#		near_100_results = (np.abs(near_100) < 1).nonzero()[0]
-#		print('\nanalytical u_d near omega * t = 100')
-#		print(f'%-22s%-25s' % ('u_d / U', 'omega * t'))
-#		for i in near_100_results:
-#			print(f'%-22f%-25f' % (analytical_u[i] / self.__max_velocity,
-#								 self.__angular_freq * t[i]))
-#		analytical_result = analytical_u[(np.abs(self.__angular_freq * t - numerical_result)).argmin()]
-#		print(f'\ndifference between closest numerical and analytical u_d = %f\n'
-#			  % (np.abs(numerical_result - analytical_result)))
-
-		return numerical_u, numerical_w, numerical_t, analytical_ud, \
-			   analytical_wd, analytical_u, analytical_w
+		return np.array(numerical_u), np.array(numerical_w), \
+			   np.array(interpd_t[1:])
 
 	def model(self, t, y):
 		x, z = y[:2]
